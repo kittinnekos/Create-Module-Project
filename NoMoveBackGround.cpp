afb1f3be45664dafe00e9238cca94bdff1e4375f
kittinnekos <131129020+kittinnekos@users.noreply.github.com>
2023-12-03 17:01:41 +0900

    [fix]動かない背景描画のコード修正（動作確認済み）

--- NoMoveBackGround.cpp
+++ NoMoveBackGround.cpp
@@ -16,4 +16,22 @@ void NoMoveBackGround::SetAndGetPositionBackGround(int x, int y, int width, int 
 	//セッター
 	image.SetPosition(x, y);
 	image.SetSize(width, height);
+
+	//ゲッター
+	sizetuple = image.GetSize();
+	postuple = image.GetPosition();
+	Width = get<0>(sizetuple);
+	Height = get<1>(sizetuple);
+	X = get<0>(postuple);
+	Y = get<1>(postuple);
+}
+
+void NoMoveBackGround::LoadBackGround(const TCHAR* _T(ImagePath)) 
+{
+	Handle = LoadGraph(ImagePath);
+}
+
+void NoMoveBackGround::DrawBackGround() 
+{
+	DrawExtendGraph(X, Y, Width, Height, Handle, true);
 }
